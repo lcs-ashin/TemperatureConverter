99fdb9e15a13f75e8295b5531567f50cd7e02d6b
lcs-ashin <ashin@lcs.on.ca>
2021-11-30 21:31:53 -0500

    Using if statement, made a text that changes by the degree

--- TemperatureConverter/ContentView.swift
+++ TemperatureConverter/ContentView.swift
@@ -54,6 +54,22 @@ struct ContentView: View {
             Text(String(format: "%.1f", degreeFahrenheit))
                 .font(.title2.bold())
     
+                .padding()
+            
+            if degreeCelsius >= -50.0 && degreeCelsius < -20.0 {
+                Text("It's freezing outside! 🥶")
+            } else if degreeCelsius >= -20.0 && degreeCelsius < 0.0 {
+                Text("Dress warmly!")
+            } else if degreeCelsius >= 0.0 && degreeCelsius < 10.0 {
+                Text("Need a coat!")
+            } else if degreeCelsius >= 10.0 && degreeCelsius < 20.0 {
+                Text("Not very cold, not very hot... Perfect!")
+            } else if degreeCelsius >= 20.0 && degreeCelsius < 30.0 {
+                Text("Warm weather! Wear thin layers.")
+            } else if degreeCelsius >= 30.0 && degreeCelsius <= 50.0 {
+                Text("Never go out! It's too hot. 🥵")
+            }
+            
             Spacer()
       
         }
